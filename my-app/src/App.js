9e5245e0e2c43f79e16b410fdbaa24dc9f4ad575
quanjiang.li <quanjiang.li@bianlifeng.com>
2019-10-12 10:11:07 +0800

    增加react-redux

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,26 +1,48 @@
-import React from 'react';
+import React , { Component }from 'react';
+import { connect } from 'react-redux'
 import logo from './logo.svg';
 import './App.css';
+import Clock from './components/Clock'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component{
+    constructor(props){
+        super(props)
+    }
+
+    componentDidMount() {
+        console.log(this.props)
+    }
+
+    render() {
+        const { PayIncrease, PayDecrease } = this.props;
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <p>{this.props.tiger}</p>
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <Clock />
+                    <button onClick={PayIncrease}>升职加薪</button>
+                    <button onClick={PayDecrease}>迟到罚款</button>
+                </header>
+            </div>
+        );
+    }
+
 }
 
-export default App;
+//需要渲染什么数据
+function mapStateToProps(state) {
+    return {
+        tiger: state
+    }
+}
+//需要触发什么行为
+function mapDispatchToProps(dispatch) {
+    return {
+        PayIncrease: () => dispatch({ type: 'ADD' }),
+        PayDecrease: () => dispatch({ type: 'MINUS' })
+    }
+}
+
+export default App = connect(mapStateToProps, mapDispatchToProps)(App);
