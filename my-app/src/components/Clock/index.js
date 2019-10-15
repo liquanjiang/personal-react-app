1932a0219637d3581e0f961e850424cc210b8527
quanjiang.li <quanjiang.li@bianlifeng.com>
2019-10-15 12:00:01 +0800

    尝试添加修饰符，编译成功

---
+++ my-app/src/components/Clock/index.js
@@ -0,0 +1,44 @@
+import React, {Component} from 'react'
+import store from '../../store'
+import './clock.css'
+import { observer } from 'mobx-react'
+
+ @observer class Clock extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            date: new Date(),
+            tiger:store.getState()
+        }
+        store.subscribe(() => {
+            this.setState({tiger:store.getState() })
+        })
+    }
+
+    componentDidMount() {
+        this.timer = setInterval(() => {
+            this.tick()
+        }, 1000)
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.timer)
+    }
+
+    tick() {
+        this.setState({
+            date: new Date(),
+        })
+    }
+
+    render() {
+        const { tiger } = this.state;
+        return (
+            <div className={'clocked'}>
+                <div>{this.state.date.toLocaleTimeString()}</div>
+                <div>{tiger}</div>
+            </div>
+        )
+    }
+}
+export default Clock
---
+++ my-app/src/components/SelectItems/index.js
@@ -0,0 +1,80 @@
+import React, {Component} from 'react'
+import store from '../../store/store'
+import './select.css'
+import { observer } from 'mobx-react'
+
+@observer class Clock extends Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            array: [
+                {
+                    name: '选项一',
+                    value: 1
+                },
+                {
+                    name: '选项二',
+                    value: 2
+                },
+                {
+                    name: '选项三',
+                    value: 3
+                },
+                {
+                    name: '选项四',
+                    value: 4
+                },
+            ],
+            tiger:store.getState(),
+            showUl: false,
+            selectedValue: 1
+        }
+        store.subscribe(() => {
+            this.setState({tiger:store.getState() })
+        })
+    }
+
+    componentDidMount() {
+
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.timer)
+    }
+
+    showList = () =>{
+        this.setState({showUl: true})
+    }
+
+    hideList = () => {
+        this.setState({showUl: false})
+    }
+
+    selectValue = (value) => {
+        this.setState({selectedValue : value, showUl: false})
+    }
+
+    render() {
+        const { tiger, array, showUl, selectedValue } = this.state;
+
+        const list =  array.map((item, index) =>
+            <li className={ item.value === selectedValue ? 'active' : ''}
+                onClick={() => this.selectValue(item.value)}
+            >
+                {item.name}
+            </li>)
+
+        return (
+            <div className={'clock'}  tabIndex={-1} onBlur={this.hideList}>
+                <div className={'content'} onClick={this.showList}>
+                    {selectedValue}
+                    <div className={'triangle'}></div>
+                </div>
+                { showUl ? <ul>{list}</ul> : null }
+            </div>
+        )
+    }
+}
+
+export default  Clock
